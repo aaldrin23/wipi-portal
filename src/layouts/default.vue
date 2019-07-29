164b174b90a614c81ca109b989c69f308f1eaa16
Aldrin Arabis <arabis.aldrin@yandex.com>
2019-07-30 00:22:10 +0800

    Working chap login

---
+++ src/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-footer absolute>
+      <v-icon left>mdi-copyright</v-icon>WiPI-Home 2019-2000
+    </v-footer>
+  </v-app>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VeeValidate from 'vee-validate'
+import Vue from 'vue';
+import App from './App.vue';
+import VeeValidate from 'vee-validate';
 
 import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import router from './router';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VeeValidate)
+Vue.use(VeeValidate);
 
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+	vuetify,
+	router,
+	render: h => h(App)
+}).$mount('#app');
